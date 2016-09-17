06d18591cab938d9b7e1097064a64a21545cff93
Peter Wilson <pw12nb@brocku.ca>
2016-09-17 05:42:27 -0400

    made the animations fancier

--- Flock/app/src/main/java/brockbadgers/flock/LoginActivity.java
+++ Flock/app/src/main/java/brockbadgers/flock/LoginActivity.java
@@ -1,11 +1,16 @@
 package brockbadgers.flock;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,6 +22,8 @@ import android.os.Handler;
 public class LoginActivity extends AppCompatActivity {
 
     final int LOAD_DELAY = 800;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final String TAG = LoginActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,15 +54,49 @@ public class LoginActivity extends AppCompatActivity {
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                /* Create an Intent that will start the Menu-Activity. */
-                                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                                LoginActivity.this.startActivity(mainIntent);
-                                LoginActivity.this.finish();
+                                openCameraShowPreview();
                             }
                         }, LOAD_DELAY);
                     }
                 }
         );
+    }
+
+    public void reqCamPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.CAMERA},
+                1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCameraShowPreview();
+                } else {
+                    //
+                    Log.d(TAG, "DENIED");
+                }
+        }
+    }
+
+
+    private void openCameraShowPreview() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            /* Create an Intent that will start the Menu-Activity. */
+            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+            LoginActivity.this.startActivity(mainIntent);
+            LoginActivity.this.finish();
+        }
     }
 
 }
