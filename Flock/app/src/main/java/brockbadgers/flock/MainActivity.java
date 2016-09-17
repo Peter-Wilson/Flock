2659b8bca59cf4d7a2bcb1c226d23fd5685daee5
fzxt <fahadahmad41@hotmail.com>
2016-09-17 01:46:57 -0400

    edit google-services.json

--- Flock/app/src/main/java/brockbadgers/flock/MainActivity.java
+++ Flock/app/src/main/java/brockbadgers/flock/MainActivity.java
@@ -5,20 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp app = FirebaseApp.getInstance();
+
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
