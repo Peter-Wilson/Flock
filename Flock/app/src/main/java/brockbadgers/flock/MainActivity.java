b1ed9b1b95fd031db35a3ea97a638fa267982b4e
RileyDavidson <riley.max32@gmail.com>
2016-09-17 04:29:05 -0400

    Can now see your location and the main activiy is a map

--- Flock/app/src/main/java/brockbadgers/flock/MainActivity.java
+++ Flock/app/src/main/java/brockbadgers/flock/MainActivity.java
@@ -1,29 +1,67 @@
 package brockbadgers.flock;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener , GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleMap map; //the map shown in the map fragment
+    GoogleApiClient mGoogleApiClient; //google api client for location services
+    boolean googleClientConnected = false; //if the google api client is connected
+    boolean waitForConnect = false; //whether to wait to populate the map until google api client is connected
+    Location currentLoc; //the currently used location
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp app = FirebaseApp.getInstance();
+        setContentView(R.layout.activity_map);
+
+        //Set up google api client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
 
 
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        ((MapFragment)getFragmentManager().findFragmentById(R.id.mapFrag)).getMapAsync(this);
+
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
@@ -54,5 +92,74 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void setUpMap() {
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true); //show the "current location" button in the top right
+        map.setOnInfoWindowClickListener(this); //set on click callback for the info window
+        map.clear(); //clear any markers currently on the map
+
+        currentLoc = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        //Starting location is the current location
+        LatLng startingLatLng = new LatLng(currentLoc.getLatitude(), currentLoc.getLongitude());
+
+
+
+        //Set up starting camera position
+        CameraPosition startingPos = new CameraPosition.Builder()
+                .target(startingLatLng)
+                .zoom(13)
+                .build();
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(startingPos));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        if(googleClientConnected) {
+            //If the google client is already connected, load the layout now
+            setUpMap();
+        } else {
+            //Otherwise wait for it to finish connecting
+            waitForConnect = true;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        googleClientConnected = true;
+        setUpMap();
+        //If the map finished setting up before the connection was established, load the data now
+        if(waitForConnect){
+            waitForConnect = false;
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("GoogleClientApi", connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
     }
 }
